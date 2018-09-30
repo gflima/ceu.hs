7339868e151bcabbb6ed8151fe9f358c9e981288
Guilherme F. Lima <gflima@sdf.org>
2018-09-30 11:18:31 -0300

    Minor syntax cleanup.

--- Ceu.hs
+++ Ceu.hs
@@ -12,20 +12,17 @@ type ID = String
 type Val = Int
 
 -- Environment.
-
 type Env = [ (ID,Val) ]         -- head of the list is the most recent bind
 
 envSet :: String -> Val -> Env -> Env
 envSet id val env = (id,val) : env
 
 envGet :: String -> Env -> (Maybe Val)
-envGet id env =
-  case env of
-    [] -> Nothing
-    (id',val') : env' -> if id==id' then (Just val') else (envGet id env')
+envGet id [] = Nothing
+envGet id ((id',val') : env) = if id == id' then (Just val')
+                                            else (envGet id env)
 
--- Expressions.
-
+-- Expression.
 data Exp
   = Const Val
   | Read ID
@@ -84,6 +81,7 @@ data Stmt
 
 -- Description.
 type Desc = (Stmt, Lvl, Maybe Evt, Env)
+
 desc1 :: Desc -> Stmt
 desc1 (p, n, e, env) = p
 desc2 :: Desc -> Lvl
@@ -145,8 +143,8 @@ nst1Adv d f
   = let res = (nst1 d) in
       case res of
         Nothing -> Nothing
-        otherwise -> let d' = (fromJust res) in
-                       Just (f (desc1 d'), desc2 d', desc3 d', desc4 d')
+        otherwise -> let (p, n, e, env) = (fromJust res) in
+                       Just (f p, n, e, env)
 
 -- Single nested transition.
 nst1 :: Desc -> Maybe Desc
@@ -173,7 +171,7 @@ nst1 (Seq Break q, n, Nothing, env)  -- seq-brk
 nst1 (Seq p q, n, Nothing, env)      -- seq-adv
   = nst1Adv (p, n, Nothing, env) (\p' -> Seq p' q)
 
-nst1 (If exp p q, n, Nothing, env)
+nst1 (If exp p q, n, Nothing, env)   -- if-true/false
   = let v = (evalExp env exp) in
       case v of
         Nothing -> Nothing
