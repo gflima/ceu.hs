86bccafac60ad1c75366f305632225f13de39588
Guilherme F. Lima <gflima@sdf.org>
2018-10-10 04:41:05 -0300

    Add CeuGrammar: Program datatype and check functions

---
+++ CeuGrammar.hs
@@ -0,0 +1,158 @@
+module CeuGrammar where
+
+-- Primitive types.
+type Evt = Int                  -- event identifier
+type ID  = String               -- variable identifier
+type Val = Int                  -- value
+
+-- Expression.
+data Expr
+  = Const Val                   -- constant
+  | Read ID                     -- contents
+  | Umn Expr                    -- unary minus
+  | Add Expr Expr               -- addition
+  | Sub Expr Expr               -- subtraction
+  deriving (Eq, Show)
+
+infixl 6 `Add`                  -- `Add` associates to the left
+infixl 6 `Sub`                  -- `Sub` associates to the left
+
+-- Program (pg 5).
+data Stmt
+  = Block [ID] Stmt             -- declaration block
+  | Write ID Expr               -- assignment statement
+  | AwaitExt Evt                -- await external event
+  | AwaitInt Evt                -- await internal event
+  | EmitInt Evt                 -- emit internal event
+  | Break                       -- loop escape
+  | If Expr Stmt Stmt           -- conditional
+  | Seq Stmt Stmt               -- sequence
+  | Loop Stmt                   -- infinite loop
+  | Every Evt Stmt              -- event iteration
+  | And Stmt Stmt               -- par/and statement
+  | Or Stmt Stmt                -- par/or statement
+  | Fin Stmt                    -- finalization statement
+  | Nop                         -- dummy statement (internal)
+  | CanRun Int                  -- wait for stack level (internal)
+  | Restore Int                 -- restore environment (internal)
+  | Loop' Stmt Stmt             -- unrolled Loop (internal)
+  | And' Stmt Stmt              -- unrolled And (internal)
+  | Or' Stmt Stmt               -- unrolled Or (internal)
+  deriving (Eq, Show)
+
+infixr 1 `Seq`                  -- `Seq` associates to the right
+infixr 0 `Or`                   -- `Or` associates to the right
+infixr 0 `And`                  -- `And` associates to the right
+
+-- Shows expression.
+showExpr :: Expr -> String
+showExpr expr = case expr of
+  (Const n)   -> show n
+  (Read v)    -> v
+  (Umn e)     -> "-(" ++ showExpr e ++ ")"
+  (Add e1 e2) -> "(" ++ showExpr e1 ++ "+" ++ showExpr e2 ++ ")"
+  (Sub e1 e2) -> "(" ++ showExpr e1 ++ "-" ++ showExpr e2 ++ ")"
+
+-- Shows list of variables.
+showVars :: [ID] -> String
+showVars vars = case vars of
+  []       -> ""
+  (v:[])   -> v
+  (v:vars) -> v ++ "," ++ showVars vars
+
+-- Shows program.
+showProg :: Stmt -> String
+showProg stmt = case stmt of
+  Block vars p   -> "{" ++ (if null vars then ""
+                               else showVars vars ++ ": ")
+                      ++ showProg p ++ "}"
+  Write var expr -> var ++ "=" ++ showExpr expr
+  AwaitExt e     -> "?E" ++ show e
+  AwaitInt e     -> "?" ++ show e
+  EmitInt e      -> "!" ++ show e
+  Break          -> "break"
+  If expr p q    -> "(if " ++ showExpr expr ++ " then "
+                      ++ showProg p ++ " else " ++ showProg q ++ ")"
+  Seq p q        -> showProg p ++ "; " ++ showProg q
+  Loop p         -> "(loop " ++ showProg p ++ ")"
+  Every e p      -> "(every " ++ show e ++ " " ++ showProg p ++ ")"
+  And p q        -> "(" ++ showProg p ++ " && " ++ showProg q ++ ")"
+  Or p q         -> "(" ++ showProg p ++ " || " ++ showProg q ++ ")"
+  Fin p          -> "(fin " ++ showProg p ++ ")"
+  Nop            -> "nop"
+  CanRun n       -> "@canrun(" ++ show n ++ ")"
+  Restore n      -> "@restore(" ++ show n ++ ")"
+  Loop' p q      -> "(" ++ showProg p ++ " @loop " ++ showProg q ++ ")"
+  And' p q       -> "(" ++ showProg p ++ " @&& " ++ showProg q ++ ")"
+  Or' p q        -> "(" ++ showProg p ++ " @|| " ++ showProg q ++ ")"
+
+-- Checks if program is valid.
+checkProg :: Stmt -> Bool
+checkProg stmt = case stmt of
+  Block _ p -> checkProg p
+  If _ p q  -> checkProg p && checkProg q
+  Seq p q   -> checkProg p && checkProg q
+  Loop p    -> checkLoop (Loop p) && checkProg p
+  Every e p -> checkEvery (Every e p) && checkProg p
+  And p q   -> checkProg p && checkProg q
+  Or p q    -> checkProg p && checkProg q
+  Fin p     -> checkFin (Fin p) && checkProg p
+  Loop' p q -> checkLoop (Loop' p q) && checkProg q
+  And' p q  -> checkProg p && checkProg q
+  Or' p q   -> checkProg p && checkProg q
+  _         -> True
+
+-- Receives a Loop or Loop' statement and checks whether all execution paths
+-- in its body lead to an occurrence of a matching-Break/AwaitExt/Every.
+checkLoop :: Stmt -> Bool
+checkLoop loop = case loop of
+  Loop body    -> checkLoop' False body
+  Loop' _ body -> checkLoop' False body
+  _            -> error "checkLoop: expected Loop or Loop'"
+  where
+    checkLoop' :: Bool -> Stmt -> Bool
+    checkLoop' ignBrk stmt = case stmt of
+      AwaitExt _ -> True
+      Break      -> not ignBrk
+      Every _ _  -> True
+      Block _ p  -> checkLoop' ignBrk p
+      If _ p q   -> checkLoop' ignBrk p && checkLoop' ignBrk q
+      Seq p q    -> checkLoop' ignBrk p || checkLoop' ignBrk q
+      Loop p     -> checkLoop' True p
+      And p q    -> checkLoop' ignBrk p && checkLoop' ignBrk q
+      Or p q     -> checkLoop' ignBrk p && checkLoop' ignBrk q
+      Fin p      -> False       -- always run in zero time
+      Loop' p q  -> checkLoop' True p && checkLoop' True q
+      And' p q   -> checkLoop' ignBrk p && checkLoop' ignBrk q
+      Or' p q    -> checkLoop' ignBrk p && checkLoop' ignBrk q
+      _          -> False
+
+-- Receives a Fin or Every statement and checks whether it does not contain
+-- any occurrences of Loop/Break/Await*/Every/Fin.
+checkFin :: Stmt -> Bool
+checkFin finOrEvery = case finOrEvery of
+  Fin body     -> checkFin' body
+  Every _ body -> checkFin' body
+  _            -> error "checkFin: expected Fin or Every"
+  where
+    checkFin' :: Stmt -> Bool
+    checkFin' stmt = case stmt of
+      Break      -> False
+      AwaitInt _ -> False
+      AwaitExt _ -> False
+      Every _ _  -> False
+      Fin _      -> False
+      Loop _     -> False
+      Loop' _ _  -> False
+      Block _ p  -> checkFin' p
+      If _ p q   -> checkFin' p && checkFin' q
+      Seq p q    -> checkFin' p && checkFin' q
+      And p q    -> checkFin' p && checkFin' q
+      Or p q     -> checkFin' p && checkFin' q
+      And' p q   -> checkFin' p && checkFin' q
+      Or' p q    -> checkFin' p && checkFin' q
+      _          -> True
+
+-- Alias for checkFin.
+checkEvery :: Stmt -> Bool
+checkEvery = checkFin
---
+++ CeuGrammarSpec.hs
@@ -0,0 +1,146 @@
+module CeuGrammarSpec where
+
+import CeuGrammar
+import Test.Hspec
+
+main :: IO ()
+main = hspec $ do
+  --------------------------------------------------------------------------
+  describe "checkLoop -- matching-Break/AwaitExt/Every in all paths" $ do
+
+    -- atomic statements --
+    checkLoopIt (Loop (Write "x" (Const 0)))      False
+    checkLoopIt (Loop (AwaitExt 0))               True
+    checkLoopIt (Loop (AwaitInt 0))               False
+    checkLoopIt (Loop (EmitInt 0))                False
+    checkLoopIt (Loop (Break))                    True
+    checkLoopIt (Loop (Nop))                      False
+    checkLoopIt (Loop (CanRun 0))                 False
+    checkLoopIt (Loop (Restore 0))                False
+    checkLoopIt (Loop' Nop (Write "x" (Const 0))) False
+    checkLoopIt (Loop' Nop (AwaitExt 0))          True
+    checkLoopIt (Loop' Nop (AwaitInt 0))          False
+    checkLoopIt (Loop' Nop (EmitInt 0))           False
+    checkLoopIt (Loop' Nop (Break))               True
+    checkLoopIt (Loop' Nop (Nop))                 False
+    checkLoopIt (Loop' Nop (CanRun 0))            False
+    checkLoopIt (Loop' Nop (Restore 0))           False
+
+    -- compound statements --
+    checkLoopIt (Loop (Block [] (Block [] Break)))               True
+    checkLoopIt (Loop (Block [] (Block [] Nop)))                 False
+
+    checkLoopIt (Loop (If (Const 0) Break Nop))                  False
+    checkLoopIt (Loop (If (Const 0) (Fin Nop) Nop))              False
+    checkLoopIt (Loop (If (Const 0) (Every 0 Nop) (AwaitExt 0))) True
+
+    checkLoopIt (Loop (Nop `Seq` Nop `Seq` Break `Seq` Nop))     True
+    checkLoopIt (Loop (Nop `Seq` Nop `Seq` (Loop Break)))        False
+    checkLoopIt (Loop (Break `Seq` Loop Nop))                    True
+    checkLoopIt (Loop (Nop `Seq` Loop Nop))                      False
+
+    checkLoopIt (Loop (Loop (Loop (AwaitExt 0))))                True
+    checkLoopIt (Loop (Loop Break))                              False
+    checkLoopIt (Loop (Loop (Loop Break)))                       False
+    checkLoopIt (Loop (Loop Break `Seq` Loop Break))             False
+    checkLoopIt (Loop (Loop (AwaitExt 0) `Seq` Loop Nop))        True
+    checkLoopIt (Loop (Loop (Seq Break Break)))                  False
+
+    checkLoopIt (Loop (Nop `And` Nop `And` Nop))                 False
+    checkLoopIt (Loop (Break `And'` Nop `And` Nop))              False
+    checkLoopIt (Loop (Break `And` Every 0 Nop `And'` Break))    True
+    checkLoopIt (Loop (Nop `Or` Nop `Or` Nop))                   False
+    checkLoopIt (Loop (Nop `Or'` AwaitExt 0 `Or` Nop))           False
+    checkLoopIt (Loop (Every 0 Nop `Or` AwaitExt 0 `Or` Break))  True
+
+    checkLoopIt (Loop (Nop `And'` Nop))                          False
+    checkLoopIt (Loop (Break `And'` Nop))                        False
+    checkLoopIt (Loop (Break `And'` (Every 0 Nop)))              True
+    checkLoopIt (Loop (Nop `Or'` Nop))                           False
+    checkLoopIt (Loop (Nop `Or'` (AwaitExt 0)))                  False
+    checkLoopIt (Loop ((Every 0 Nop) `Or'` (AwaitExt 0)))        True
+
+    -- Fin always run in zero time.
+    checkLoopIt (Loop (Fin Nop))                                 False
+    checkLoopIt (Loop (Fin Break))                               False
+    checkLoopIt (Loop (Fin (AwaitExt 0)))                        False
+    checkLoopIt (Loop (Fin (Every 0 Nop)))                       False
+
+  --------------------------------------------------------------------------
+  describe "checkFin/Every -- no Loop/Break/Await*/Every/Fin:" $ do
+
+    -- atomic statements --
+    checkFinIt (Fin (Write "x" (Const 0))) True
+    checkFinIt (Fin (AwaitExt 0))          False
+    checkFinIt (Fin (AwaitInt 0))          False
+    checkFinIt (Fin (EmitInt 0))           True
+    checkFinIt (Fin (Break))               False
+    checkFinIt (Fin (Nop))                 True
+    checkFinIt (Fin (CanRun 0))            True
+    checkFinIt (Fin (Restore 0))           True
+
+    -- compound statements --
+    checkFinIt (Fin (Block [] Nop))                               True
+    checkFinIt (Fin (Block [] (Every 0 Nop)))                     False
+    checkFinIt (Fin (If (Const 0) (Loop Break) (Nop)))            False
+    checkFinIt (Fin (If (Const 0) (Write "x" (Const 0)) (Nop)))   True
+    checkFinIt (Fin (Nop `Seq` Nop `Seq` (AwaitExt 0) `Seq` Nop)) False
+    checkFinIt (Fin (Nop `Seq` Nop `Seq` (EmitInt 0) `Seq` Nop))  True
+    checkFinIt (Fin (Loop (AwaitInt 0)))                          False
+    checkFinIt (Fin (Loop (AwaitExt 0)))                          False
+    checkFinIt (Fin (Loop' Nop Nop))                              False
+    checkFinIt (Fin (Loop' Nop (AwaitExt 0)))                     False
+    checkFinIt (Fin (Nop `And` Nop `And` (EmitInt 0)))            True
+    checkFinIt (Fin (Fin Nop `And'` Nop `And` (EmitInt 0)))       False
+    checkFinIt (Fin ((Restore 0) `Or` Nop `Or` (EmitInt 0)))      True
+    checkFinIt (Fin (Break `Or'` Nop `Or` (EmitInt 0)))           False
+
+  --------------------------------------------------------------------------
+  describe "checkProg -- program is valid" $ do
+
+    -- atomic statements --
+    checkProgIt (Write "c" (Const 0)) True
+    checkProgIt (AwaitExt 0)          True
+    checkProgIt (AwaitInt 0)          True
+    checkProgIt (EmitInt 0)           True
+    checkProgIt (Break)               True
+    checkProgIt (Nop)                 True
+    checkProgIt (CanRun 0)            True
+    checkProgIt (Restore 0)           True
+
+    -- compound statements --
+    checkProgIt (Block [] Nop)           True
+    checkProgIt (If (Const 0) Nop Break) True
+    checkProgIt (Seq Break Nop)          True
+    checkProgIt (Loop Break)             True
+    checkProgIt (Loop Nop)               False
+    checkProgIt (Every 0 Nop)            True
+    checkProgIt (Every 0 (Fin Nop))      False
+    checkProgIt (And Break Nop)          True
+    checkProgIt (Or Nop (EmitInt 0))     True
+    checkProgIt (Fin Nop)                True
+    checkProgIt (Fin (Fin Nop))          False
+    checkProgIt (Loop' Nop Break)        True
+    checkProgIt (Loop' Nop Nop)          False
+    checkProgIt (And' Break Nop)         True
+    checkProgIt (Or' Nop (EmitInt 0))    True
+
+    -- misc --
+    checkProgIt (Nop `Seq` (Fin (Loop Break)))                      False
+    checkProgIt (Nop `Seq` (Fin (Loop Nop)))                        False
+    checkProgIt (Block [] (Fin (Every 0 Nop)))                      False
+    checkProgIt (Loop (Loop Break))                                 False
+    checkProgIt (Loop (Loop (Seq Break Break)))                     False
+    checkProgIt (AwaitInt 0 `Seq` (Fin Break) `Or` Nop)             False
+    checkProgIt (AwaitInt 0 `Seq` (Every 0 (Fin Nop)) `Or` Nop)     False
+    checkProgIt (Loop (Nop `Or` Loop (Loop (Loop (AwaitExt 0)))))   False
+    checkProgIt (Loop (Break `Or` Loop (Loop (Loop (AwaitExt 0))))) True
+
+      where
+        checkIt ck p b   =
+          (it ((if b then "pass" else "fail") ++ ": " ++ showProg p) $
+            (ck p) `shouldBe` b)
+        checkLoopIt p b  = checkIt checkLoop p b
+        checkFinIt p b   = checkIt checkFin p b
+        checkEveryIt p b = checkIt checkEvery p b
+        checkProgIt p b  = checkIt checkProg p b
