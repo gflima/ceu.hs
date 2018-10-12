aee3fc90550a38d60123af24794f9dfe5f2fb117
Francisco Sant'Anna <francisco.santanna@gmail.com>
2018-10-12 09:58:52 -0300

    Converts FullGrammar to Grammar.

--- Ceu/FullGrammar.hs
+++ Ceu/FullGrammar.hs
@@ -2,41 +2,24 @@ module Ceu.FullGrammar where
 
 import qualified Ceu.Grammar as G
 
--- Primitive types.
-type Evt = Int                  -- event identifier
-type ID  = String               -- variable identifier
-type Val = Int                  -- value
-
--- Expression.
-data Expr
-  = Const Val                   -- constant
-  | Read ID                     -- variable read
-  | Umn Expr                    -- unary minus
-  | Add Expr Expr               -- addition
-  | Sub Expr Expr               -- subtraction
-  deriving (Eq, Show)
-
-infixl 6 `Add`                  -- `Add` associates to the left
-infixl 6 `Sub`                  -- `Sub` associates to the left
-
 -- Program (pg 5).
 data Stmt
   = Block Stmt                  -- declaration block
-  | Var ID
-  | Write ID Expr               -- assignment statement
-  | AwaitExt Evt                -- await external event
-  | AwaitInt Evt                -- await internal event
-  | EmitInt Evt                 -- emit internal event
+  | Var G.ID
+  | Write G.ID G.Expr           -- assignment statement
+  | AwaitExt G.Evt              -- await external event
+  | AwaitInt G.Evt              -- await internal event
+  | EmitInt G.Evt               -- emit internal event
   | Break                       -- loop escape
-  | If Expr Stmt Stmt           -- conditional
+  | If G.Expr Stmt Stmt         -- conditional
   | Seq Stmt Stmt               -- sequence
   | Loop Stmt                   -- infinite loop
-  | Every Evt Stmt              -- event iteration
+  | Every G.Evt Stmt            -- event iteration
   | And Stmt Stmt               -- par/and statement
   | Or Stmt Stmt                -- par/or statement
   | Finalize Stmt               -- finalization statement
   | Async Stmt
-  | Block' [ID] Stmt
+  | Block' [G.ID] Stmt
   | Nop
   deriving (Eq, Show)
 
@@ -44,9 +27,9 @@ infixr 1 `Seq`                  -- `Seq` associates to the right
 infixr 0 `Or`                   -- `Or` associates to the right
 infixr 0 `And`                  -- `And` associates to the right
 
--- Removes (Var id) stmts and maps (Block p) -> (Block' [ids] p)
+-- remVars: Removes (Var id) stmts and maps (Block p) -> (Block' [ids] p)
 
-remVars :: Stmt -> ([ID], Stmt)
+remVars :: Stmt -> ([G.ID], Stmt)
 remVars (Block p)      = ([], Block' ids' p') where (ids', p') = remVars p
 remVars (Var id')      = ([id'], Nop)
 remVars (If exp p1 p2) = remVars2 p1 p2 (\p1' p2' -> If exp p1' p2')
@@ -59,14 +42,32 @@ remVars (Finalize p)   = remVars1 p (\p' -> Finalize p')
 remVars (Async p)      = remVars1 p (\p' -> Async p')
 remVars q              = ([], q)
 
-remVars1 :: Stmt -> (Stmt->Stmt) -> ([ID], Stmt)
+remVars1 :: Stmt -> (Stmt->Stmt) -> ([G.ID], Stmt)
 remVars1 p f = (ids', f p') where (ids',p') = remVars p
 
-remVars2 :: Stmt -> Stmt -> (Stmt->Stmt->Stmt) -> ([ID], Stmt)
+remVars2 :: Stmt -> Stmt -> (Stmt->Stmt->Stmt) -> ([G.ID], Stmt)
 remVars2 p1 p2 f = (ids1'++ids2', f p1' p2') where
                   (ids1',p1') = remVars p1
                   (ids2',p2') = remVars p2
 
--- create block enclosing everything
--- remove remVars
--- convert to grammar
+-- toGrammar: Converts full -> basic
+
+toGrammar :: Stmt -> G.Stmt
+toGrammar p = toG p' where
+  (_, p') = remVars (Block (Seq (Var "ret") p))
+
+  toG :: Stmt -> G.Stmt
+  toG (Write id exp) = G.Write id exp
+  toG (AwaitExt e)   = G.AwaitExt e
+  toG (AwaitInt e)   = G.AwaitInt e
+  toG Break          = G.Break
+  toG (If e p1 p2)   = G.If e (toG p1) (toG p2)
+  toG (Seq p1 p2)    = G.Seq (toG p1) (toG p2)
+  toG (Loop p)       = G.Loop (toG p)
+  toG (Every e p)    = G.Every e (toG p)
+  toG (And p1 p2)    = G.And (toG p1) (toG p2)
+  toG (Or p1 p2)     = G.Or (toG p1) (toG p2)
+  --toG (Finalize p)   = ...
+  --toG (Async Stmt)   = ...
+  toG (Block' ids p) = G.Block ids (toG p)
+  toG Nop            = G.Nop
--- tests/Ceu/FullGrammarSpec.hs
+++ tests/Ceu/FullGrammarSpec.hs
@@ -1,5 +1,6 @@
 module Ceu.FullGrammarSpec (main, spec) where
 
+import qualified Ceu.Grammar as G
 import Ceu.FullGrammar
 import Test.Hspec
 
@@ -16,17 +17,29 @@ spec = do
       `shouldBe` (["x"], Nop)
 
     it "x = 1;" $ do
-      remVars (Write "x" (Const 1))
-      `shouldBe` ([], Write "x" (Const 1))
+      remVars (Write "x" (G.Const 1))
+      `shouldBe` ([], Write "x" (G.Const 1))
 
     it "var x; x = 1" $ do
-      remVars (Seq (Var "x") (Write "x" (Const 1)))
-      `shouldBe` (["x"], Seq Nop (Write "x" (Const 1)))
+      remVars (Seq (Var "x") (Write "x" (G.Const 1)))
+      `shouldBe` (["x"], Seq Nop (Write "x" (G.Const 1)))
 
     it "var x || x = 1" $ do
-      remVars (Or (Var "x") (Write "x" (Const 1)))
-      `shouldBe` (["x"], Or Nop (Write "x" (Const 1)))
+      remVars (Or (Var "x") (Write "x" (G.Const 1)))
+      `shouldBe` (["x"], Or Nop (Write "x" (G.Const 1)))
 
     it "do var x; x = 1 end" $ do
-      remVars (Block (Seq (Var "x") (Write "x" (Const 1))))
-      `shouldBe` ([], (Block' ["x"] (Seq Nop (Write "x" (Const 1)))))
+      remVars (Block (Seq (Var "x") (Write "x" (G.Const 1))))
+      `shouldBe` ([], (Block' ["x"] (Seq Nop (Write "x" (G.Const 1)))))
+
+  --------------------------------------------------------------------------
+  describe "toGrammar" $ do
+
+    it "var x;" $ do
+      toGrammar (Var "x")
+      `shouldBe` G.Block ["ret","x"] (G.Seq G.Nop G.Nop)
+
+    it "do var x; x = 1 end" $ do
+      toGrammar (Block (Seq (Var "x") (Write "x" (G.Const 1))))
+      `shouldBe` G.Block ["ret"] (G.Seq G.Nop (G.Block ["x"] (G.Seq G.Nop (G.Write "x" (G.Const 1)))))
+
