d68ce6a27526a6c2c57fa74e82a0957f47516315
Francisco Sant'Anna <francisco.santanna@gmail.com>
2018-10-12 12:41:53 -0300

    Small adjusts.

--- Ceu/FullGrammar.hs
+++ Ceu/FullGrammar.hs
@@ -66,7 +66,7 @@ remVar _         = error "remVar: expected enclosing top-level block"
 
 remSpawn :: Stmt -> Stmt
 remSpawn (Block p)           = Block (remSpawn p)
-remSpawn (If e p1 p2)        = If e (remSpawn p1) (remSpawn p2)
+remSpawn (If exp p1 p2)      = If exp (remSpawn p1) (remSpawn p2)
 remSpawn (Seq (Spawn p1) p2) = Or (Seq (remSpawn p1) AwaitFor) (remSpawn p2)
 remSpawn (Seq p1 p2)         = Seq (remSpawn p1) (remSpawn p2)
 remSpawn (Loop p)            = Loop (remSpawn p)
@@ -93,37 +93,37 @@ chkSpawn p = case p of
   notS p         = True
 
   chkS :: Stmt -> Bool
-  chkS (Block p)           = (notS p) && (chkS p)
-  chkS (If e p1 p2)        = (notS p1) && (notS p2) && (chkS p1) && (chkS p2)
-  chkS (Seq p1 p2)         = (chkS p1) && (notS p2) && (chkS p2)
-  chkS (Loop p)            = (notS p) && (chkS p)
-  chkS (Every e p)         = (notS p) && (chkS p)
-  chkS (And p1 p2)         = (notS p1) && (notS p2) && (chkS p1) && (chkS p2)
-  chkS (Or p1 p2)          = (notS p1) && (notS p2) && (chkS p1) && (chkS p2)
-  chkS (Spawn p)           = (notS p) && (chkS p)
-  chkS (Finalize p)        = (notS p) && (chkS p)
-  chkS (Async p)           = (notS p) && (chkS p)
-  chkS (Block' ids p)      = (notS p) && (chkS p)
-  chkS (Fin' p)            = (notS p) && (chkS p)
-  chkS _                   = True
+  chkS (Block p)      = (notS p) && (chkS p)
+  chkS (If exp p1 p2) = (notS p1) && (notS p2) && (chkS p1) && (chkS p2)
+  chkS (Seq p1 p2)    = (chkS p1) && (notS p2) && (chkS p2)
+  chkS (Loop p)       = (notS p) && (chkS p)
+  chkS (Every e p)    = (notS p) && (chkS p)
+  chkS (And p1 p2)    = (notS p1) && (notS p2) && (chkS p1) && (chkS p2)
+  chkS (Or p1 p2)     = (notS p1) && (notS p2) && (chkS p1) && (chkS p2)
+  chkS (Spawn p)      = (notS p) && (chkS p)
+  chkS (Finalize p)   = (notS p) && (chkS p)
+  chkS (Async p)      = (notS p) && (chkS p)
+  chkS (Block' ids p) = (notS p) && (chkS p)
+  chkS (Fin' p)       = (notS p) && (chkS p)
+  chkS _              = True
 
 -- remAwaitFor: Converts AwaitFor into (AwaitExt inputForever)
 
 remAwaitFor :: Stmt -> Stmt
-remAwaitFor (Block p)           = Block (remAwaitFor p)
-remAwaitFor (If e p1 p2)        = If e (remAwaitFor p1) (remAwaitFor p2)
-remAwaitFor (Seq p1 p2)         = Seq (remAwaitFor p1) (remAwaitFor p2)
-remAwaitFor (Loop p)            = Loop (remAwaitFor p)
-remAwaitFor (Every e p)         = Every e (remAwaitFor p)
-remAwaitFor (And p1 p2)         = And (remAwaitFor p1) (remAwaitFor p2)
-remAwaitFor (Or p1 p2)          = Or (remAwaitFor p1) (remAwaitFor p2)
-remAwaitFor (Spawn p)           = Spawn (remAwaitFor p)
-remAwaitFor (Finalize p)        = Finalize (remAwaitFor p)
-remAwaitFor (Async p)           = Async (remAwaitFor p)
-remAwaitFor (Block' ids p)      = Block' ids (remAwaitFor p)
-remAwaitFor (Fin' p)            = Fin' (remAwaitFor p)
-remAwaitFor AwaitFor            = AwaitExt G.inputForever
-remAwaitFor p                   = p
+remAwaitFor (Block p)      = Block (remAwaitFor p)
+remAwaitFor (If exp p1 p2) = If exp (remAwaitFor p1) (remAwaitFor p2)
+remAwaitFor (Seq p1 p2)    = Seq (remAwaitFor p1) (remAwaitFor p2)
+remAwaitFor (Loop p)       = Loop (remAwaitFor p)
+remAwaitFor (Every e p)    = Every e (remAwaitFor p)
+remAwaitFor (And p1 p2)    = And (remAwaitFor p1) (remAwaitFor p2)
+remAwaitFor (Or p1 p2)     = Or (remAwaitFor p1) (remAwaitFor p2)
+remAwaitFor (Spawn p)      = Spawn (remAwaitFor p)
+remAwaitFor (Finalize p)   = Finalize (remAwaitFor p)
+remAwaitFor (Async p)      = Async (remAwaitFor p)
+remAwaitFor (Block' ids p) = Block' ids (remAwaitFor p)
+remAwaitFor (Fin' p)       = Fin' (remAwaitFor p)
+remAwaitFor AwaitFor       = AwaitExt G.inputForever
+remAwaitFor p              = p
 
 -- toGrammar: Converts full -> basic
 
@@ -135,7 +135,7 @@ toGrammar p = toG $ remAwaitFor $ remSpawn $ chkSpawn $ remVar (Block (Seq (Var 
   toG (AwaitInt e)   = G.AwaitInt e
   toG (EmitInt e)    = G.EmitInt e
   toG Break          = G.Break
-  toG (If e p1 p2)   = G.If e (toG p1) (toG p2)
+  toG (If exp p1 p2) = G.If exp (toG p1) (toG p2)
   toG (Seq p1 p2)    = G.Seq (toG p1) (toG p2)
   toG (Loop p)       = G.Loop (toG p)
   toG (Every e p)    = G.Every e (toG p)
