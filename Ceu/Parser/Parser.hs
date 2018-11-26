dbbf86c4b96b105327717e64b742609da404a40a
Francisco Sant'Anna <francisco.santanna@gmail.com>
2018-11-26 21:49:47 -0200

    Started parser using `Parsec`.

---
+++ Ceu/Parser/Parser.hs
@@ -0,0 +1,49 @@
+module Ceu.Parser.Parser where
+
+import Prelude hiding (exp)
+import Control.Monad (void)
+import Data.Char (isLower, isLetter, isDigit)
+import Control.Applicative ((<|>), many)
+
+import Text.Parsec.String (Parser)
+import Text.Parsec.String.Char (oneOf, char, digit, satisfy, string)
+import Text.Parsec.String.Combinator (many1, choice, chainl1)
+
+import Ceu.Globals
+import Ceu.Full.Grammar
+
+s :: Parser ()
+s = void $ many $ oneOf " \n\t"
+
+num :: Parser Int
+num = do
+    s
+    n <- many1 digit
+    return (read n)
+
+id_var :: Parser String
+id_var = do
+    s
+    first <- satisfy isLower
+    rest  <- many $ satisfy (\c -> isDigit c || isLetter c || c == '_')
+    return (first:rest)
+
+id_int = id_var
+
+exp_const = do
+    v <- num
+    return $ Const v
+
+exp :: Parser Exp
+exp = do
+    e <- exp_const
+    return e
+
+stmt_escape = do
+    void <- string "escape"
+    e    <- exp
+    return $ Escape Nothing (Just e)
+
+stmt = do
+    s <- stmt_escape
+    return s
---
+++ tests/Parser/Parser.hs
@@ -0,0 +1,65 @@
+module Test.Parser (main, spec) where
+
+import Test.Hspec
+
+import Text.Parsec.String (Parser)
+import FunctionsAndTypesForParsing
+
+import Ceu.Parser.Parser
+import Ceu.Globals
+import Ceu.Full.Grammar
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+
+    describe "num" $ do
+        it "''" $
+            parse num "\n "
+            `shouldBe` Left "(line 2, column 2):\nunexpected end of input\nexpecting digit"
+        it "''" $
+            parse num ""
+            `shouldBe` Left "(line 1, column 1):\nunexpected end of input\nexpecting digit"
+        it "1" $
+            parse num "1"
+            `shouldBe` Right 1
+        it "10" $
+            parse num "10"
+            `shouldBe` Right 10
+        it "a" $
+            parse num "a"
+            `shouldBe` Left "(line 1, column 1):\nunexpected \"a\"\nexpecting digit"
+
+    describe "id_var_evt" $ do
+        it "''" $
+            parse id_var "\n "
+            `shouldBe` Left "(line 2, column 2):\nunexpected end of input"
+        it "''" $
+            parse id_var ""
+            `shouldBe` Left "(line 1, column 1):\nunexpected end of input"
+        it "id" $
+            parse id_var "id"
+            `shouldBe` Right "id"
+        it "1" $
+            parse id_var "1"
+            `shouldBe` Left "(line 1, column 1):\nunexpected \"1\""
+        it "var" $
+            parse id_var "var"
+            `shouldBe` Left "TODO: id cannot be a keyword"
+
+    describe "stmt" $ do
+        describe "escape" $ do
+            it "escape 0" $
+                parse stmt_escape "escape 0"
+                `shouldBe` Right (Escape Nothing (Just (Const 0)))
+
+    where
+        parse :: Parser a -> String -> Either String a
+        parse rule input =
+            let v = regularParse rule input in
+                case v of
+                    (Right v') -> Right v'
+                    (Left  v') -> Left (show v')
+                
