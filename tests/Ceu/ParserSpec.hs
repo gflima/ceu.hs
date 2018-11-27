115ed1e204def94cef9d36930f5e3abe723366cd
Francisco Sant'Anna <francisco.santanna@gmail.com>
2018-11-26 23:28:05 -0200

    Fix previous commit.

--- tests/Ceu/ParserSpec.hs
+++ tests/Ceu/ParserSpec.hs
@@ -1,4 +1,4 @@
-module Test.RunSpec (main, spec) where
+module Test.ParserSpec (main, spec) where
 
 import Test.Hspec
 
@@ -15,14 +15,49 @@ main = hspec spec
 spec :: Spec
 spec = do
 
-    describe "escape" $ do
-        it "escape 0" $
-            run "escape 0"
-            `shouldBe` Right (Escape Nothing (Just (Const 0)))
+    describe "num" $ do
+        it "''" $
+            parse num "\n "
+            `shouldBe` Left "(line 2, column 2):\nunexpected end of input\nexpecting digit"
+        it "''" $
+            parse num ""
+            `shouldBe` Left "(line 1, column 1):\nunexpected end of input\nexpecting digit"
+        it "1" $
+            parse num "1"
+            `shouldBe` Right 1
+        it "10" $
+            parse num "10"
+            `shouldBe` Right 10
+        it "a" $
+            parse num "a"
+            `shouldBe` Left "(line 1, column 1):\nunexpected \"a\"\nexpecting digit"
+
+    describe "id_var_evt" $ do
+        it "''" $
+            parse id_var "\n "
+            `shouldBe` Left "(line 2, column 2):\nunexpected end of input"
+        it "''" $
+            parse id_var ""
+            `shouldBe` Left "(line 1, column 1):\nunexpected end of input"
+        it "id" $
+            parse id_var "id"
+            `shouldBe` Right "id"
+        it "1" $
+            parse id_var "1"
+            `shouldBe` Left "(line 1, column 1):\nunexpected \"1\""
+        it "var" $
+            parse id_var "var"
+            `shouldBe` Left "TODO: id cannot be a keyword"
+
+    describe "stmt" $ do
+        describe "escape" $ do
+            it "escape 0" $
+                parse stmt_escape "escape 0"
+                `shouldBe` Right (Escape Nothing (Just (Const 0)))
 
     where
-        run :: String -> Either String a
-        run rule input =
+        parse :: Parser a -> String -> Either String a
+        parse rule input =
             let v = regularParse rule input in
                 case v of
                     (Right v') -> Right v'
