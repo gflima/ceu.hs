58fc23dde44cfc5b0c77efd23803a381c5e1eb94
Francisco Sant'Anna <francisco.santanna@gmail.com>
2018-11-27 10:47:52 -0200

    Add `Run` tests: from source code to full evaluation.

--- tests/Ceu/RunSpec.hs
+++ tests/Ceu/RunSpec.hs
@@ -6,8 +6,10 @@ import Text.Parsec.String (Parser)
 import FunctionsAndTypesForParsing
 
 import Ceu.Parser.Parser
-import Ceu.Grammar.Globals
-import Ceu.Grammar.Full.Grammar
+import Ceu.Eval                 (Outs)
+import Ceu.Grammar.Globals      (Val)
+import Ceu.Grammar.Full.Grammar (In)
+import Ceu.Grammar.Full.Eval    (evalFullProg)
 
 main :: IO ()
 main = hspec spec
@@ -16,15 +18,22 @@ spec :: Spec
 spec = do
 
     describe "escape" $ do
-        it "escape 0" $
-            run "escape 0"
-            `shouldBe` Right (Escape Nothing (Just (Const 0)))
+        it "escape 1" $
+            run "escape 1" []
+            `shouldBe` Right (1, [[]])
+        it "escape" $
+            run "escape" []
+            `shouldBe` Right (1, [[]])
 
     where
-        run :: String -> Either String Stmt
-        run input =
+        run :: String -> [In] -> Either String (Val,[Outs])
+        run input hist =
             let v = regularParse stmt input in
                 case v of
-                    (Right v') -> Right v'
+                    (Right stmt) ->
+                        let ret = evalFullProg stmt hist in
+                            case ret of
+                                (Left errs)           -> Left $ concatMap (\s->s++"\n") errs
+                                (Right r@(res,outss)) -> Right r
                     (Left  v') -> Left (show v')
                 
