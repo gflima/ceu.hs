d53bf2b952076bdac193eeb4a26c9eb999a03067
Francisco Sant'Anna <francisco.santanna@gmail.com>
2018-12-11 08:32:20 -0200

    Add `If`, `AwaitExt`, `EmitExt` to `Gen`.

--- Ceu/Code/Gen.hs
+++ Ceu/Code/Gen.hs
@@ -1,36 +1,81 @@
 module Ceu.Code.Gen where
 
 import Debug.Trace
+import Ceu.Grammar.Globals
+import Ceu.Grammar.Ann.All
 import Ceu.Grammar.Exp  (Exp(..))
 import Ceu.Grammar.Stmt (Stmt(..))
+import qualified Ceu.Code.N as N
 
-data State = State { iden  :: Int
-                   , spc   :: String
-                   , traps :: [Int]
+-------------------------------------------------------------------------------
+
+data State = State { spc   :: String
+                   , traps :: [Stmt All]
+                   , trail :: Int
                    } -- deriving (Show)   
 
-cmd :: String -> String
-cmd str = str ++ ";\n"
+z :: State
+z = State { spc   = ""
+          , traps = []
+          , trail = 0
+          }
+
+ident :: State -> State
+ident g = g{ spc=(spc g)++"  " }
+
+-------------------------------------------------------------------------------
+
+ocmd :: State -> String -> String
+ocmd g str = (spc g) ++ str ++ ";\n"
+
+oblk :: State -> String -> String
+oblk g str = (spc g) ++ "{\n" ++ str ++ (spc g) ++ "}\n"
+
+-------------------------------------------------------------------------------
 
 expr :: Exp ann -> String
 expr (Const _ n) = show n
 
-stmt :: (Show ann) => Stmt ann -> String
-stmt p = aux g p --(traceShowId p)
-        where
-            g = State { iden  = 0
-                      , spc   = "  "
-                      , traps = []
-                      }
+-------------------------------------------------------------------------------
 
-aux :: State -> Stmt ann -> String
+label :: Stmt All -> String -> String
+label s lbl = "CEU_LABEL_" ++ (show $ toN s) ++ "_" ++ lbl
+
+stmt :: Stmt Source -> String
+stmt p = aux (ident z) (N.add p) --(traceShowId p)
+
+aux :: State -> Stmt All -> String
 aux g (Var      _ var p)     = aux g p
-aux g (Write    _ var e)     = cmd $ (spc g) ++ "CEU_APP.root." ++ var ++ " = " ++ (expr e)
-aux g (AwaitExt _ "FOREVER") = cmd $ (spc g) ++ "return"
+aux g (Write    _ var exp)   = ocmd g $ "CEU_APP.root." ++ var ++ " = " ++ (expr exp)
+aux g (AwaitExt _ "FOREVER") = ocmd g $ "return"
 aux g (Seq      _ p1 p2)     = (aux g p1) ++ (aux g p2)
-aux g (Trap     _ p)         = cmd $ (aux g' p) ++ "case Trap_" ++ (show $ iden g) ++ ":"
-                               where
-                                g' = g { iden  = (iden g)+1
-                                       , traps = (iden g):(traps g)
-                                       }
-aux g (Escape _ k)           = cmd $ (spc g) ++ "CEU_GOTO(Trap_" ++ (show $ (traps g)!!k) ++ ")"
+
+aux g s@(AwaitExt _ ext) =
+    (ocmd g $ "_ceu_mem->_trails[" ++ trl ++ "].evt" ++ " = " ++ evt) ++
+    (ocmd g $ "_ceu_mem->_trails[" ++ trl ++ "].lbl" ++ " = " ++ lbl) ++
+    (ocmd g $ "return") ++
+    (ocmd z $ "case " ++ lbl)
+    where
+        trl = show $ trail g
+        evt = "CEU_INPUT_" ++ ext
+        lbl = label s ("AwaitExt_" ++ ext)
+
+aux g (EmitExt  _ ext exp) =
+    ocmd g $ "CEU_OUTPUT_" ++ ext ++ "(" ++ exp' ++ ")"
+    where
+        exp' = case exp of
+            Nothing  -> ""
+            (Just v) -> expr v
+
+aux g (If _ exp p1 p2) =
+    (spc g) ++ "if (" ++ (expr exp) ++ ")\n" ++
+                    oblk g (aux (ident g) p1) ++
+    (spc g) ++ "else\n" ++
+                    oblk g (aux (ident g) p2)
+
+aux g s@(Trap _ p) =
+    ocmd z $ (aux g' p) ++ "case " ++ (label s "Trap") ++ ":"
+    where
+        g' = g{ traps = s:(traps g) }
+
+aux g (Escape _ k) = ocmd g $ "CEU_GOTO(" ++ (label ((traps g)!!k) "Trap") ++ ")"
