23903e52a7dba2f8070c4f5ccebe2ac7e5869c60
Francisco Sant'Anna <francisco.santanna@gmail.com>
2019-02-14 12:26:18 -0200

    (*) missing file

---
+++ Ceu/Grammar/Full/Compile/Match.hs
@@ -0,0 +1,24 @@
+module Ceu.Grammar.Full.Compile.Match where
+
+import Debug.Trace
+import Ceu.Grammar.Full.Full
+
+compile :: Stmt -> Stmt
+compile p = stmt p
+stmt :: Stmt -> Stmt
+stmt (Class z cls vars ifc)     = Class z cls vars (stmt ifc)
+stmt (Inst  z cls tps  imp)     = Inst  z cls tps  (stmt imp)
+stmt (Write z loc exp)          = Match z loc (expr exp) (Nop z) (Err z (Number z 1))
+stmt (CallS z exp1 exp2)        = CallS z (expr exp1) (expr exp2)
+stmt (If z exp p1 p2)           = Match z (LCons ["Bool","True"] LUnit) (expr exp)
+                                          (stmt p1) (stmt p2)
+stmt (Seq z p1 p2)              = Seq   z (stmt p1) (stmt p2)
+stmt (Loop z p)                 = Loop  z (stmt p)
+stmt (Scope z p)                = Scope z (stmt p)
+stmt (Ret z exp)                = Ret   z (expr exp)
+stmt p                          = p
+
+expr :: Exp -> Exp
+expr (Call z e1 e2)             = Call z (expr e1) (expr e2)
+expr (Func z tp p)              = Func z tp (stmt p)
+expr e                          = e
