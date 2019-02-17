520a885e3b637b4fbf13c47c2148d7dbb704dcb4
Francisco Sant'Anna <francisco.santanna@gmail.com>
2019-02-16 22:16:11 -0200

    (+) BookSpec

---
+++ tests/Ceu/BookSpec.hs
@@ -0,0 +1,112 @@
+module Ceu.BookSpec (main, spec) where
+
+import Test.Hspec
+import Data.Bool             (bool)
+import Debug.Trace
+import Text.Parsec           (eof, parse)
+
+import Ceu.Eval              (Exp(..))
+import Ceu.Grammar.Ann       (annz)
+import Ceu.Grammar.Full.Eval (go, prelude)
+import Ceu.Parser.Stmt       (stmt)
+
+main :: IO ()
+main = hspec spec
+
+spec :: Spec
+spec = do
+
+    --describe "TODO:" $ do
+
+-------------------------------------------------------------------------------
+
+    -- TODO-3: square : Float -> Float
+
+    describe "Chapter 1:" $ do
+
+      it "square" $
+        (run True $
+          unlines [
+            "func square (x) : (Int -> Int) do",
+            "   return x * x",
+            "end",
+            "return square 3"
+           ])
+        `shouldBe` Right (Number 9)
+
+      it "smaller" $
+        (run True $
+          unlines [
+            "func smaller (x,y) : ((Int,Int) -> Int) do",
+            "   if x <= y then",
+            "     return x",
+            "   else",
+            "     return y",
+            "   end",
+            "end",
+            "return (smaller(10,5)) + (smaller(1,4))"
+           ])
+        `shouldBe` Right (Number 6)
+
+      it "square/smaller" $
+        (run True $
+          unlines [
+            "func square (x) : (Int -> Int) do",
+            "   return x * x",
+            "end",
+            "func smaller (x,y) : ((Int,Int) -> Int) do",
+            "   if x <= y then",
+            "     return x",
+            "   else",
+            "     return y",
+            "   end",
+            "end",
+            "return square (smaller (5, 3+4))"
+           ])
+        `shouldBe` Right (Number 25)
+
+      it "three" $
+        (run True $
+          unlines [
+            "func three (x) : (Int -> Int) do",
+            "   return 3",
+            "end",
+            "return three 10"
+           ])
+        `shouldBe` Right (Number 3)
+
+      it "infinity" $
+        (run True $
+          unlines [
+            "func infinity () : (() -> Int) do",
+            "   return (infinity()) + 1",
+            "end",
+            "return infinity ()"
+           ])
+        `shouldBe` Right (Error 2)
+
+      it "three" $
+        (run True $
+          unlines [
+            "func three (x) : (Int -> Int) do",
+            "   return 3",
+            "end",
+            "func infinity () : (() -> Int) do",
+            "   return (infinity()) + 1",
+            "end",
+            "return three (infinity ())"
+           ])
+        `shouldBe` Right (Error 2)
+
+-------------------------------------------------------------------------------
+
+    where
+        run :: Bool -> String -> Either String Exp
+        run withPrelude input =
+            let v = parse (stmt <* eof) "" input in
+                case v of
+                    (Right p) ->
+                      case go $ bool Prelude.id (prelude annz) withPrelude $ p of
+                        (Left errs) -> Left $ concatMap (\s->s++"\n") errs
+                        (Right exp) -> Right exp
+                    (Left  v') -> Left (show v')
