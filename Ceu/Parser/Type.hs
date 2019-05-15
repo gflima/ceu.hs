f07a3b94107e193a3f24a85c4b6648edcceb66d1
Francisco Sant'Anna <francisco.santanna@gmail.com>
2019-05-15 17:26:16 -0300

    (>) multiple types for implements

--- Ceu/Parser/Type.hs
+++ Ceu/Parser/Type.hs
@@ -1,5 +1,8 @@
 module Ceu.Parser.Type where
 
+import Control.Exception        (assert)
+
+import Text.Parsec.Prim         ((<|>), (<?>), try)
 import Text.Parsec.Prim         ((<|>), (<?>), try)
 import Text.Parsec.String       (Parser)
 import Text.Parsec.Prim         (many)
@@ -10,6 +13,8 @@ import Ceu.Parser.Token         (tk_sym, tk_key, tk_class, tk_var, tk_data_hier)
 
 import Ceu.Grammar.Globals      (ID_Var, ID_Class)
 import Ceu.Grammar.Type         (Type(..))
+
+singleton x = [x]
 
 type_0 :: Parser Type
 type_0 = do
@@ -55,15 +60,18 @@ pType = type_1 <|> try type_V <|> try type_0 <|> try type_N <|> try type_F
 pTypeIfc :: Parser Type
 pTypeIfc = do
   tp  <- pType
-  ifc <- optionMaybe ((,) <$> (try (tk_key "where") *> tk_var) <*> (tk_key "implements" *> tk_class))
+  ifc <- optionMaybe ((,) <$> (try (tk_key "where") *> ((singleton <$> tk_var) <|> list tk_var)) <*> (tk_key "implements" *> ((singleton <$> tk_class) <|> list tk_class)))
   return $ implements tp ifc
 
-implements :: Type -> Maybe (ID_Var,ID_Class) -> Type
+implements :: Type -> Maybe ([ID_Var],[ID_Class]) -> Type
 implements tp Nothing = tp
-implements tp (Just (var,cls)) = aux (var,cls) tp where
-  aux :: (ID_Var,ID_Class) -> Type -> Type
-  aux (var,cls) (TypeV var' []) | var==var' = TypeV var [cls]
-  aux (var,cls) Type0           = Type0
-  aux (var,cls) (TypeD hier)    = TypeD hier
-  aux (var,cls) (TypeF inp out) = TypeF (aux (var,cls) inp) (aux (var,cls) out)
-  aux (var,cls) (TypeN ts)      = TypeN $ map (aux (var,cls)) ts
+implements tp (Just (l1,l2)) = aux tp $ assert (length l1 == length l2) (zip l1 l2)
+  where
+    aux :: Type -> [(ID_Var,ID_Class)] -> Type
+    aux tp l = foldr f tp l
+
+    f :: (ID_Var,ID_Class) -> Type -> Type
+    f (var,cls) (TypeV var' []) | var==var' = TypeV var [cls]
+    f (var,cls) (TypeF inp out) = TypeF (f (var,cls) inp) (f (var,cls) out)
+    f (var,cls) (TypeN ts)      = TypeN $ map (f (var,cls)) ts
+    f _         tp              = tp
